49a9fff94fb4bceb2cdc649a4cec76a7ced4ee0d
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-09-27 21:03:18 +0800

    09270105

---
+++ eeit36/src/tw/brad/myjava/Brad06.java
@@ -0,0 +1,15 @@
+package tw.brad.myjava;
+
+public class Brad06 {
+
+	public static void main(String[] args) {
+		// 浮點數 => float(單) / double(雙) => 精確度
+		float v1 = (float)12.0; // 12.3 帶有小數點的數值 => double
+		double v2 = 12.3;
+		float v3 = 10;
+		System.out.println(v1);
+		System.out.println(v2);
+		System.out.println(v3);
+	}
+
+}
