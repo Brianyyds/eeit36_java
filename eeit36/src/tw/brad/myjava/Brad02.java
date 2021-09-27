2b8ad72abffdcbb3077a473e5489a3a6bf62889d
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-09-27 13:32:11 +0800

    09271332

---
+++ eeit36/src/tw/brad/myjava/Brad02.java
@@ -0,0 +1,10 @@
+package tw.brad.myjava;
+
+public class Brad02 {
+
+	public static void main(String[] args) {
+		System.out.println("Brad1");
+		System.out.println("Brad2");
+	}
+
+}
---
+++ eeit36/src/tw/brad/myjava/Brad03.java
@@ -0,0 +1,11 @@
+package tw.brad.myjava;
+
+public class Brad03 {
+
+	public static void main(String[] args) {
+		byte var1;
+		byte var2 = 123; 
+		byte var3 = 123;
+	}
+
+}
