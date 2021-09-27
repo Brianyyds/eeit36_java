194f17a13c17256b7dfc83ae9a3b8d8303cc8171
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-09-27 17:04:29 +0800

    09270101

--- eeit36/src/tw/brad/myjava/Brad03.java
+++ eeit36/src/tw/brad/myjava/Brad03.java
@@ -6,6 +6,21 @@ public class Brad03 {
 		byte var1;
 		byte var2 = 123; 
 		byte var3 = 123;
+		// 自訂變數名稱: [a-zA-Z$_][a-zA-z0-9$_]*
+		byte a;
+		byte $$$ = 100;
+		System.out.println($$$);
+		byte $_$ = 12;
+		byte a_$$$ = 34;
+		System.out.println($_$);
+		// 整數 => 正負
+		// byte, short, int, long
+		// byte => 2^8 => 256 => -128 ~ 127	
+		// short => 2^16 => 65,536 => -32,768 ~ 32,767
+		// int => 2^32 約 42.9億 => 十位數 => 4G
+		// long => 2^64
+		byte var4 = -128;
+		// 宣告 => 變數的儲存空間大小
 		
 	}
 
