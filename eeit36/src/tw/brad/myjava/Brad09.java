4a3e6463337185371b00ab257a0f32cd88c911fe
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-09-28 09:46:27 +0800

    09280946

--- eeit36/src/tw/brad/myjava/Brad09.java
+++ eeit36/src/tw/brad/myjava/Brad09.java
@@ -10,6 +10,8 @@ public class Brad09 {
 		
 		// 字串 => 字元成一串 => 物件
 		// 字元 => 一個字元
+		// '' 單引號刮註一個字元 => 字元 => 基本型態
+		// "" 雙引號刮註一個字串 => 字串 => 物件型態
 		char c1 = 'a';	// 97
 		char c2 = 100;	// 'a'
 		char c3 = '資';
@@ -17,8 +19,9 @@ public class Brad09 {
 		System.out.println(c2);
 		System.out.println(c3);
 		
-		
-
+		boolean b1 = true;	// 1bit
+		boolean b2 = false;	
+		System.out.println(b2);
 	}
 
 }
---
+++ eeit36/src/tw/brad/myjava/Brad10.java
@@ -0,0 +1,18 @@
+package tw.brad.myjava;
+
+public class Brad10 {
+
+	public static void main(String[] args) {
+		// if 判斷式
+		// if (boolean){程式區塊}
+		
+		int a = 10;
+		if (a != 10) {
+			System.out.println("OK");
+		}else {
+			System.out.println("XX");
+		}
+
+	}
+
+}
