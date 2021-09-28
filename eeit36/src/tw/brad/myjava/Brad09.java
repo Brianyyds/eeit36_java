13c6380a130bbfceda532523f201b5d53ccfadf0
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-09-28 09:26:34 +0800

    09280926

---
+++ eeit36/src/tw/brad/myjava/Brad09.java
@@ -0,0 +1,24 @@
+package tw.brad.myjava;
+
+public class Brad09 {
+
+	public static void main(String[] args) {
+		// byte, short, int, long
+		// float, double
+		// char 字元, 2^16 => 0 ~ 65535 
+		// boolean
+		
+		// 字串 => 字元成一串 => 物件
+		// 字元 => 一個字元
+		char c1 = 'a';	// 97
+		char c2 = 100;	// 'a'
+		char c3 = '資';
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		
+		
+
+	}
+
+}
