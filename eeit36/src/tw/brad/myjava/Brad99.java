b65c0a3dab106eaaaf9567cfed8418d674087385
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-09-28 15:37:09 +0800

    09281537

--- eeit36/src/tw/brad/myjava/Brad99.java
+++ eeit36/src/tw/brad/myjava/Brad99.java
@@ -3,12 +3,13 @@ package tw.brad.myjava;
 public class Brad99 {
 
 	public static void main(String[] args) {
-		int r;
+		int r, newi;
 		for (int k = 0; k < 2; k++) {
 			for (int j = 1; j <= 9 ; j++) {
 				for (int i = 2; i <= 5 ; i++) {
-					r = i * j;
-					System.out.printf("%d x %d = %d\t", i, j, r);
+					newi = i + k * 4;
+					r = newi * j;
+					System.out.printf("%d x %d = %d\t", newi, j, r);
 				}
 				System.out.println();
 			}
