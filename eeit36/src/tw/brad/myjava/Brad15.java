5f55b6943988b2cb54dd3c65916d714c5137b437
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-09-28 14:29:43 +0800

    09281429

---
+++ eeit36/src/tw/brad/myjava/Brad15.java
@@ -0,0 +1,23 @@
+package tw.brad.myjava;
+
+public class Brad15 {
+
+	public static void main(String[] args) {
+		// for
+		
+		for (int i = 0; i<10; i++) {
+			System.out.println(i);
+		}
+		// --------------
+		System.out.println("----");
+		
+		int i = 0;
+		for ( ; i<10 ; i++) {
+			System.out.println(i);
+		}
+		
+		
+		
+	}
+
+}
