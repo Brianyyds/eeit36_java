00fec17648c1007dae7f6af9d06632ccb626e640
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-09-28 15:00:22 +0800

    09281500

---
+++ eeit36/src/tw/brad/myjava/Brad99.java
@@ -0,0 +1,14 @@
+package tw.brad.myjava;
+
+public class Brad99 {
+
+	public static void main(String[] args) {
+		int r;
+		for (int i = 2; i <= 5 ; i++) {
+			r = i * 1;
+			System.out.printf("%d x %d = %d\t", i, 1, r);
+		}
+		
+	}
+
+}
