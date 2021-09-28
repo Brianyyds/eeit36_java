89c6ebdacef7e5ef4435fda8e2fdf4526d4eb3ce
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-09-28 16:17:19 +0800

    09281617

---
+++ eeit36/src/tw/brad/myjava/Brad16.java
@@ -0,0 +1,19 @@
+package tw.brad.myjava;
+
+public class Brad16 {
+
+	public static void main(String[] args) {
+		// 陣列 Array => 物件
+		int a = 10;
+		boolean[] ary = new boolean[3];	// 元素個數 [+int]
+		System.out.println(a);
+		System.out.println(ary); // 陣列物件記憶體的位置 
+		System.out.println(ary[0]);
+		System.out.println(ary[1]);
+		System.out.println(ary[2]);
+
+		// 完成初始化 => 變數具體存在 => 清楚明白
+		
+	}
+
+}
