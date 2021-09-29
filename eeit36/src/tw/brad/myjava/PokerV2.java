aa42a3757474a9cab1cafb973699249fd7990b52
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-09-29 14:24:15 +0800

    09291424

--- eeit36/src/tw/brad/myjava/PokerV2.java
+++ eeit36/src/tw/brad/myjava/PokerV2.java
@@ -1,29 +1,30 @@
 package tw.brad.myjava;
 
-public class PokerV1 {
+public class PokerV2 {
 
 	public static void main(String[] args) {
 		// 1. 洗牌 2. 發牌 => 4個玩家(13) 3. 理牌 + 攤牌
 		long startTime = System.currentTimeMillis();
 		int nums = 52;
 		int[] poker = new int[nums];	// poker[0] = 0, poker[1] = 0,...... poker[51] = 0
+		int brad;
+		boolean isRepeat;
 		for (int i=0; i<poker.length; i++) {
-			int brad = (int)(Math.random() * nums);	// 0 <= rand <= 51
 			
-			// 檢查機制
-			boolean isRepeat = false;
-			for (int j=0; j<i; j++) {
-				if (poker[j] == brad) {
-					isRepeat = true;
-					break;
+			do {
+				brad = (int)(Math.random() * nums);	// 0 <= rand <= 51
+				// 檢查機制
+				isRepeat = false;
+				for (int j=0; j<i; j++) {
+					if (poker[j] == brad) {
+						isRepeat = true;
+						break;
+					}
 				}
-			}
+			}while (isRepeat);
 			
-			if (!isRepeat) {
-				poker[i] = brad;	
-			}else {
-				i--;
-			}
+			poker[i] = brad;
+			
 		}
 
 		long endTime = System.currentTimeMillis();
