201d5057700d9e333e81a0991c2cf4947800996b
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-09-29 09:22:09 +0800

    09290922

--- eeit36/src/tw/brad/myjava/Brad18.java
+++ eeit36/src/tw/brad/myjava/Brad18.java
@@ -3,15 +3,15 @@ package tw.brad.myjava;
 public class Brad18 {
 
 	public static void main(String[] args) {
-		int[] p = new int[6];	// p[0] = 0, p[1] = 0, ... p[5] = 0
+		int[] p = new int[7];	// p[0] = 0, p[1] = 0, ... p[5] = 0, p[6] = 0
 		
 		for (int i=0; i<100; i++) {
-			int point = (int)(Math.random()*6);	// 0 ~ 5
+			int point = (int)(Math.random()*6) + 1;	// point => 1 ~ 6
 			p[point]++;
 		}
-		
-		for (int i=0; i<p.length; i++) {
-			System.out.printf("%d點出現%d次\n", (i+1), p[i]);
+
+		for (int i = 1; i<=6; i++) {	// i => 0, 1, 2, 3, 4, 5
+			System.out.printf("%d點出現%d次\n", i, p[i]);
 		}
 		
 	}
