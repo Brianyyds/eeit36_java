2e7006a48b5cebe04e7fa2e1b78a7e178953e0fb
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-09-30 09:15:41 +0800

    09300915

---
+++ eeit36/src/tw/brad/myjava/Brad22.java
@@ -0,0 +1,28 @@
+package tw.brad.myjava;
+
+public class Brad22 {
+
+	public static void main(String[] args) {
+		int[] a = new int[] {11,222,333};
+		System.out.println(a.length);
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		
+		System.out.println("---");
+		
+		int[] b = {5,66,777,888,9999};
+		System.out.println(b.length);
+		
+		System.out.println("---");
+		
+		int[] c;
+		int[] d;
+		
+		c = new int[] {1,2,3};
+		//d = {1,2,3};
+		
+		
+	}
+
+}
