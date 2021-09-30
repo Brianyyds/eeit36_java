f709d6726813285dfa33bb22f5c8dcb49255dfde
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-09-30 10:50:53 +0800

    09301050

--- eeit36/src/tw/brad/myjava/Brad23.java
+++ eeit36/src/tw/brad/myjava/Brad23.java
@@ -7,9 +7,10 @@ public class Brad23 {
 	public static void main(String[] args) {
 		// 1 + 2 + ... + n = ?
 		System.out.println("計算: 1 + 2 + ... + n = ?");
-		Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);	// Stream 串流 => 水管 => 水龍頭
 		System.out.print("n = ");
 		int n = scanner.nextInt();
+		scanner.close();
 		
 		int counter = 1, sum = 0;
 		while (counter <= n) {
