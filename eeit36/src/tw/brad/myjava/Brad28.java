f330c8bba7e074af97c4b62ba6eaf08ccc41cb15
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-10-04 13:43:12 +0800

    10041343

--- eeit36/src/tw/brad/myjava/Brad28.java
+++ eeit36/src/tw/brad/myjava/Brad28.java
@@ -2,10 +2,11 @@ package tw.brad.myjava;
 
 import javax.swing.JFrame;
 
-public class Brad28 extends JFrame{
+public class Brad28 extends JFrame{ // Brad28 extends JFrame => Brad28的物件 is-a JFrame
 	public Brad28() {
 		setSize(640,480);
 		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
 	public static void main(String[] args) {
