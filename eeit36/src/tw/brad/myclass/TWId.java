aa6fe23582aa3b8ef12d9c3242e7031ac1efbf29
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-10-05 12:01:57 +0800

    10051201

---
+++ eeit36/src/tw/brad/myclass/TWId.java
@@ -0,0 +1,63 @@
+package tw.brad.myclass;
+
+public class TWId {
+	// 屬性
+	private String id;	// TWId 類別物件 has-a id 的屬性
+	
+	// TWId 的物件, 其 id 一定要有效的, 不是 null, 而且不能被變更
+
+	// Overload 多載 => 參數 => 個數 型別
+	// 無傳參數 => 沒有設限條件 => 亂數產生一組有效的 id
+	public TWId() {
+		this((int)(Math.random()*2) == 0);
+	}
+	// 依照指定 isMale (男/女) 的隨機有效 id
+	public TWId(boolean isMale) {
+		
+	}
+	// 依照指定 area (報戶口地區) 的隨機有效 id 
+	public TWId(int area) {
+		
+	}
+	
+	public TWId(boolean isMale, String area) {
+		
+	}
+	
+	public TWId(String urId) {
+		
+	}
+	
+	
+	
+	public static boolean checkTWId(String id) {
+		boolean isRight = false;
+		
+		if (id.matches("[A-Z][12][0-9]{8}")) {
+			String letterString = "ABCDEFGHJKLMNPQRSTUVXYWZIO";	// 10 - 35 indexOf() => 0, 1, 2, ...
+			char c1 = id.charAt(0);	// A
+			int pos = letterString.indexOf(c1);	// 0 - 25
+			int c12 = pos + 10;	// 10 - 35
+			int n1 = c12 / 10;	// 十位數
+			int n2 = c12 % 10;	// 個位數
+			//System.out.println(n1 + ":" + n2);
+			String s2 = id.substring(1, 2);
+			int n3 = Integer.parseInt(s2);
+			int n4 = Integer.parseInt(id.substring(2, 3));
+			int n5 = Integer.parseInt(id.substring(3, 4));
+			int n6 = Integer.parseInt(id.substring(4, 5));
+			int n7 = Integer.parseInt(id.substring(5, 6));
+			int n8 = Integer.parseInt(id.substring(6, 7));
+			int n9 = Integer.parseInt(id.substring(7, 8));
+			int n10 = Integer.parseInt(id.substring(8, 9));
+			int n11 = Integer.parseInt(id.substring(9, 10));
+			int sum = n1*1 + n2*9 + n3*8 + n4*7 + n5*6 + n6*5 +
+					n7*4 + n8*3 + n9*2 + n10*1 + n11*1;
+			
+			isRight = sum % 10 == 0;
+		}
+				
+		return isRight;
+	}
+
+}
--- eeit36/src/tw/brad/myjava/Brad32.java
+++ eeit36/src/tw/brad/myjava/Brad32.java
@@ -1,49 +1,21 @@
 package tw.brad.myjava;
 
+import tw.brad.myclass.TWId;
+
 public class Brad32 {
 
 	public static void main(String[] args) {
 		String id = "Y120239847";
-		if (checkTWId(id)) {
+		if (TWId.checkTWId(id)) {
 			System.out.println("OK");
 		}else {
 			System.out.println("XX");
 		}
-	}
-	
-	public static boolean checkTWId(String id) {
-		// 檢查的條件有哪些
-		// 第一字元為英文
-		// 第二個是1或2
-		// 由9個連續數字組成
-		// 10碼
-		boolean isRight = false;
+		System.out.println("---");
 		
-		if (id.matches("[A-Z][12][0-9]{8}")) {
-			String letterString = "ABCDEFGHJKLMNPQRSTUVXYWZIO";	// 10 - 35 indexOf() => 0, 1, 2, ...
-			char c1 = id.charAt(0);	// A
-			int pos = letterString.indexOf(c1);	// 0 - 25
-			int c12 = pos + 10;	// 10 - 35
-			int n1 = c12 / 10;	// 十位數
-			int n2 = c12 % 10;	// 個位數
-			//System.out.println(n1 + ":" + n2);
-			String s2 = id.substring(1, 2);
-			int n3 = Integer.parseInt(s2);
-			int n4 = Integer.parseInt(id.substring(2, 3));
-			int n5 = Integer.parseInt(id.substring(3, 4));
-			int n6 = Integer.parseInt(id.substring(4, 5));
-			int n7 = Integer.parseInt(id.substring(5, 6));
-			int n8 = Integer.parseInt(id.substring(6, 7));
-			int n9 = Integer.parseInt(id.substring(7, 8));
-			int n10 = Integer.parseInt(id.substring(8, 9));
-			int n11 = Integer.parseInt(id.substring(9, 10));
-			int sum = n1*1 + n2*9 + n3*8 + n4*7 + n5*6 + n6*5 +
-					n7*4 + n8*3 + n9*2 + n10*1 + n11*1;
-			
-			isRight = sum % 10 == 0;
-		}
-				
-		return isRight;
+		double d = Math.random();
+		System.out.println(d);
+		
 	}
 	
 }
