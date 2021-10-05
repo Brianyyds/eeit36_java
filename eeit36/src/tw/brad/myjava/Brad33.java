dc7c1b49f16f47deebb6a3ff0a59d6b01fb185d1
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-10-05 16:03:27 +0800

    10051603

--- eeit36/src/tw/brad/myjava/Brad33.java
+++ eeit36/src/tw/brad/myjava/Brad33.java
@@ -3,10 +3,40 @@ package tw.brad.myjava;
 public class Brad33 {
 	public static void main(String[] args) {
 		Brad331 obj1 = new Brad331();
+		obj1.m1();
 		System.out.println("---");
-		Brad332 obj2 = new Brad332(2);
+		Brad332 obj2 = new Brad332();
 		System.out.println("---");
 		Brad334 obj3 = new Brad334();
+		System.out.println("---");
+		
+		Brad331 obj4 = new Brad332();	// Brad332的物件實體 is-a Brad331
+		obj4.m1();
+		System.out.println("---");
+		Brad332 obj5 = (Brad332)obj4;
+		obj5.m1();
+		obj5.m2();
+		System.out.println("---");
+		Brad331 obj6 = new Brad333();
+		System.out.println("---");
+		
+		go(obj4);
+		go(obj6);
+		System.out.println("---");
+		
+		if (obj4 instanceof Object) {
+			System.out.println("OK");
+		}else {
+			System.out.println("XX");
+		}
+		System.out.println("---");
+		//Brad334 obj7 = (Brad334)obj4;
+		// 轉型 => 物件多型
+		
+	}
+	
+	public static void go(Brad331 obj) {
+		obj.m1();
 	}
 }
 
@@ -14,23 +44,26 @@ class Brad331 extends Object{
 	Brad331(){
 		System.out.println("Brad331()");
 	}
+	void m1() {System.out.println("Brad331:m1()");}
 }
 
 class Brad332 extends Brad331 {
-	Brad332(int a){
+	Brad332(){
 		System.out.println("Brad332()");
 	}
+	void m1() {System.out.println("Brad332:m1()");}
+	void m2() {System.out.println("Brad332:m2()");}
 }
 
 class Brad333 extends Brad331 {
 	Brad333(){
 		System.out.println("Brad333()");
 	}
+	void m1() {System.out.println("Brad333:m1()");}
 }
 
 class Brad334 extends Brad332 {
 	Brad334(){
-		super(2);
 		System.out.println("Brad334()");
 	}
 }
