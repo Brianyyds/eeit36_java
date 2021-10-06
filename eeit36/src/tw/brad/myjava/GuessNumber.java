ff45694096d235b5056cca895eedee5fff34ebbd
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-10-06 09:38:13 +0800

    10060938

---
+++ eeit36/src/tw/brad/myjava/GuessNumber.java
@@ -0,0 +1,21 @@
+package tw.brad.myjava;
+
+import javax.swing.JFrame;
+
+public class GuessNumber extends JFrame {
+	public GuessNumber() {
+		super("猜數字遊戲");
+		
+		// 配置版面
+		
+		// 處理使用者的事件
+		
+		setVisible(true);
+		setSize(640,480);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	public static void main(String[] args) {
+		new GuessNumber();
+	}
+
+}
