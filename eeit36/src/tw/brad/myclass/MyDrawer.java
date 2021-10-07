e3b2270dc227caf223ad90a297faf0850dd21489
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-10-07 11:06:52 +0800

    10071106

--- eeit36/src/tw/brad/myclass/MyDrawer.java
+++ eeit36/src/tw/brad/myclass/MyDrawer.java
@@ -1,17 +1,21 @@
 package tw.brad.myclass;
 
 import javax.swing.*;
+import javax.swing.event.MouseInputAdapter;
 import javax.swing.text.AttributeSet.ColorAttribute;
 import java.awt.*;
+import java.awt.event.*;
 
 public class MyDrawer extends JPanel {	// MyDrawer的物件實體 is-a JPanel
 	public MyDrawer() {
 		setBackground(Color.yellow);
+		
+		MyMouseListener myMouseListener = new MyMouseListener();
+		addMouseMotionListener(myMouseListener);
 	}
 
 //	@Override
 //	protected void paintComponent(Graphics g) {
-//		// TODO Auto-generated method stub
 //		super.paintComponent(g);
 //	}
 
@@ -28,4 +32,12 @@ public class MyDrawer extends JPanel {	// MyDrawer的物件實體 is-a JPanel
 		g2d.drawLine(0,0, 200,200);
 	}
 	
+	private class MyMouseListener extends MouseAdapter {
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			System.out.println(e.getX() + ":" + e.getY());
+		}
+	}
+	
 }
+
