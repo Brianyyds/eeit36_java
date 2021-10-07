d70f3e89aaf46b5b7c7ff355fe49a5e1231b1378
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-10-07 11:48:56 +0800

    10071148

--- eeit36/src/tw/brad/myclass/MyDrawer.java
+++ eeit36/src/tw/brad/myclass/MyDrawer.java
@@ -9,15 +9,16 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class MyDrawer extends JPanel {	// MyDrawer的物件實體 is-a JPanel
-	private LinkedList<HashMap<String, Integer>> line;
+	private LinkedList<LinkedList<HashMap<String, Integer>>> lines;
 	
 	public MyDrawer() {
 		setBackground(Color.yellow);
 	
-		line = new LinkedList<>();
+		lines = new LinkedList<>();
 		
 		MyMouseListener myMouseListener = new MyMouseListener();
 		addMouseMotionListener(myMouseListener);
+		addMouseListener(myMouseListener);
 	}
 
 //	@Override
@@ -35,11 +36,17 @@ public class MyDrawer extends JPanel {	// MyDrawer的物件實體 is-a JPanel
 		
 		g2d.setColor(Color.BLUE);
 		g2d.setStroke(new BasicStroke(4.123f));
-		for (int i=1; i<line.size(); i++) {
-			HashMap<String, Integer> p0 = line.get(i-1);
-			HashMap<String, Integer> p1 = line.get(i);
-			g2d.drawLine(p0.get("x"), p0.get("y"), p1.get("x"),p1.get("y"));
+
+		// 畫所有在 lines 中的每一個 line
+		for (LinkedList<HashMap<String, Integer>> line : lines) {
+			// 畫一條 line
+			for (int i=1; i<line.size(); i++) {
+				HashMap<String, Integer> p0 = line.get(i-1);
+				HashMap<String, Integer> p1 = line.get(i);
+				g2d.drawLine(p0.get("x"), p0.get("y"), p1.get("x"),p1.get("y"));
+			}			
 		}
+		
 	}
 	
 	private class MyMouseListener extends MouseAdapter {
@@ -47,9 +54,24 @@ public class MyDrawer extends JPanel {	// MyDrawer的物件實體 is-a JPanel
 		public void mouseDragged(MouseEvent e) {
 			HashMap<String, Integer> point = new HashMap<>();
 			point.put("x", e.getX()); point.put("y", e.getY());
-			line.add(point);
+			
+			// point 放在 lines 中, 最後的那一條線中
+			lines.getLast().add(point);
 			repaint();
 		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			HashMap<String, Integer> point = new HashMap<>();
+			point.put("x", e.getX()); point.put("y", e.getY());
+			
+			LinkedList<HashMap<String, Integer>> line = new LinkedList<>();
+			line.add(point);
+			
+			lines.add(line);
+		}
+		
+		
 	}
 	
 }
