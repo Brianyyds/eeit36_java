7e4389878c4d40f05f2485848c9e3fa85243a7cc
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-10-12 12:02:47 +0800

    10121202

--- eeit36/src/tw/brad/myjava/MyEditor.java
+++ eeit36/src/tw/brad/myjava/MyEditor.java
@@ -1,15 +1,24 @@
 package tw.brad.myjava;
 
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 public class MyEditor extends JFrame {
 	private JButton openBtn, saveBtn, saveAsBtn;
 	private JTextArea textArea;
+	private File openFile;
 	
 	public MyEditor() {
 		super("我的編輯器");
@@ -41,7 +50,31 @@ public class MyEditor extends JFrame {
 	}
 	
 	private void setEvent() {
-		
+		openBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				openFile();
+			}
+		});
+	}
+	
+	private void openFile() {
+		JFileChooser jFileChooser = new JFileChooser();
+		if (jFileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			openFile = jFileChooser.getSelectedFile();
+			
+			try {
+				FileInputStream fin = new FileInputStream(openFile);
+				byte[] buf = new byte[(int)openFile.length()];
+				fin.read(buf);
+				fin.close();
+				
+				textArea.setText(new String(buf));
+				
+			}catch(Exception e){
+				System.out.println("XXX");
+			}
+		}
 	}
 	
 	
