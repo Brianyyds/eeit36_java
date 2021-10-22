68fc25e3f7d7c0b889187fbfa4de9c01136eec36
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-10-22 09:19:51 +0800

    10220919

---
+++ eeit36/src/tw/brad/myjava/Brad63.java
@@ -0,0 +1,39 @@
+package tw.brad.myjava;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Brad63 {
+
+	public static void main(String[] args) {
+		
+		try {
+			URL url = new URL("https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG");
+			URLConnection connection =  url.openConnection();
+			connection.connect();
+			
+			BufferedOutputStream bout = new BufferedOutputStream(new FileOutputStream("dir1/brad.jpg"));
+			
+			BufferedInputStream bin = new BufferedInputStream(connection.getInputStream());
+			byte[] buf = new byte[4*1024];
+			int len;
+			while ( (len = bin.read(buf)) != -1) {
+				bout.write(buf, 0, len);
+			}
+			bin.close();
+			
+			bout.flush();
+			bout.close();
+			System.out.println("download OK");
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		
+		
+	}
+
+}
