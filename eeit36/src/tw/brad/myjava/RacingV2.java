b0c79c03539f352ad0e6e158a0ac043526107dc7
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-10-22 11:58:19 +0800

    10221158

---
+++ eeit36/src/tw/brad/myjava/RacingV2.java
@@ -0,0 +1,83 @@
+package tw.brad.myjava;
+
+import java.awt.GridLayout;
+import java.awt.event.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class RacingV2 extends JFrame {
+	private JButton goButton;
+	private JLabel[] lanes = new JLabel[8];
+	private Car[] cars = new Car[8];
+	
+	public RacingV2() {
+		super("Racing Game");
+		
+		setLayout(new GridLayout(9,1));
+		
+		goButton = new JButton("Go!");
+		add(goButton);
+		
+		for (int i=0; i<8; i++) {
+			lanes[i] = new JLabel( (i+1) + ". " );
+			add(lanes[i]);
+		}
+		
+		goButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				go();
+			}
+		});
+		
+		setSize(1280,480);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	private void go() {
+		for (int i=0; i<8; i++) {
+			lanes[i].setText((i+1) + ". " );
+		}		
+		for (int i=0; i<8; i++) {
+			cars[i] = new Car(i);
+			cars[i].start();
+		}
+		
+	}
+	
+	private class Car extends Thread {
+		int lane;
+		Car(int lane){this.lane = lane;}
+		@Override
+		public void run() {
+			for (int i=0; i<100; i++) {
+				if (i == 99) {
+					lanes[lane].setText(lanes[lane].getText() + "> WINNER");
+					stopGame();
+				}else {
+					lanes[lane].setText(lanes[lane].getText() + ">");
+				}
+				
+				try {
+					Thread.sleep(0 + (int)(Math.random()*200));	// 0 ~ 199
+				}catch (InterruptedException e) {
+					break;
+				}
+			}
+		}
+	}
+	
+	private void stopGame() {
+		for (int i=0; i<8; i++) {
+			cars[i].interrupt();
+		}		
+	}
+	
+	public static void main(String[] args) {
+		new RacingV2();
+	}
+
+}
