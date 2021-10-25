e603b784ae1fe9ce79493e29df93f8f3828d4467
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-10-25 14:41:20 +0800

    10251441

--- eeit36/src/tw/brad/myjava/Brad69.java
+++ eeit36/src/tw/brad/myjava/Brad69.java
@@ -1,15 +1,35 @@
 package tw.brad.myjava;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Brad69 {
 
 	public static void main(String[] args) {
 		// Driver => Connector
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("OK");
+			System.out.println("OK1");
 		} catch (ClassNotFoundException e) {
 			System.out.println(e.toString());
 		}
+		
+		try {
+			Connection connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/eeit36?user=root&password=root");
+			Statement stmt = connection.createStatement();
+			stmt.execute("INSERT INTO cust (cname,tel,birthday) VALUES ('Brad','123','1999-01-02')");
+			
+			connection.close();
+			System.out.println("OK2");
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+		}
+		
+		
+		
 	}
 
 }
