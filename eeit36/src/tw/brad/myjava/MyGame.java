d508daa07b764a40eb4b8d4d41e1bc687c3c5e69
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-10-25 11:11:24 +0800

    10251111

--- eeit36/src/tw/brad/myjava/MyGame.java
+++ eeit36/src/tw/brad/myjava/MyGame.java
@@ -8,6 +8,8 @@ import java.awt.*;
 import java.awt.image.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MyGame extends JFrame{
@@ -25,22 +27,54 @@ public class MyGame extends JFrame{
 	}
 
 	private class MyPanel extends JPanel {
+		private Timer timer;
+		private int x, y, dx, dy;
+		private int viewW, viewH;
+		
 		public MyPanel() {
 			setBackground(Color.YELLOW);
+			timer = new Timer();
+			timer.schedule(new Refresh(), 0, 16);
+			timer.schedule(new BallTask(), 1*1000, 30);
+			x = y = 0;
+			dx = dy = 12;
+		}
+		
+		private class BallTask extends TimerTask {
+			@Override
+			public void run() {
+				if (x < 0 || x + 72 >= viewW ) {
+					dx *= -1;
+				}
+				if (y < 0 || y + 72 >= viewH) {
+					dy *= -1;
+				}
+				
+				x += dx;
+				y += dy;
+				
+			}
+		}
+		
+		private class Refresh extends TimerTask {
+			@Override
+			public void run() {
+				repaint();		
+			}
 		}
 		
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
+			viewW = getWidth(); viewH = getHeight();
 			
 			try {
-				BufferedImage img = ImageIO.read(new File("dir1/ball.png"));
-				g.drawImage(img, 0, 0, null);
+				BufferedImage img = ImageIO.read(new File("dir1/ball0.png"));
+				g.drawImage(img, x, y, null);
 				
 			} catch (IOException e) {
 				System.out.println(e.toString());
 			} 
-			
 		}
 	}
 	
