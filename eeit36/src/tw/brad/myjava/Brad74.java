f52fbd49e2d65e5e7c0dbee39915cb2d08164727
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-10-25 15:00:28 +0800

    10251500

---
+++ eeit36/src/tw/brad/myjava/Brad74.java
@@ -0,0 +1,33 @@
+package tw.brad.myjava;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class Brad74 {
+
+	public static void main(String[] args) {
+		
+		Properties prop = new Properties();
+		prop.put("user", "root");
+		prop.put("password", "root");
+		
+		try {
+			Connection connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/eeit36", prop);
+			Statement stmt = connection.createStatement();
+			stmt.execute("DELETE FROM cust WHERE id = 5");
+			
+			connection.close();
+			System.out.println("OK4");
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+		}
+		
+		
+		
+	}
+
+}
