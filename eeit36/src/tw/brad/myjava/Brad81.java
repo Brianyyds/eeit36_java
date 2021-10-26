630966c911aeaf3d47375e438d8bec1f5890292f
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-10-26 11:01:12 +0800

    10261101

--- eeit36/src/tw/brad/myjava/Brad81.java
+++ eeit36/src/tw/brad/myjava/Brad81.java
@@ -5,11 +5,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Properties;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-
-import netscape.javascript.JSObject;
 
 public class Brad81 {
 
@@ -17,7 +19,6 @@ public class Brad81 {
 		String jsonData = fetchOpendata();
 		if (jsonData != null) {
 			parseOpendata(jsonData);
-			insertOpendata();
 		}
 	}
 	
@@ -48,16 +49,37 @@ public class Brad81 {
 	}
 	
 	private static void parseOpendata(String json) {
-		JSONArray root = new JSONArray(json);
-		for (int i=0; i<root.length(); i++) {
-			JSONObject row = root.getJSONObject(i);
-			String name = row.getString("Name");
-			System.out.println(name);
+		Properties prop = new Properties();
+		prop.put("user", "root");
+		prop.put("password", "root");
+		String sql = "INSERT INTO food (name,address,tel,hostwords,feature,city,town,coordinate,picurl)" + 
+				" VALUES (?,?,?,?,?,?,?,?,?)";
+
+		try (Connection connection = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/eeit36", prop)) {
+			
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+			
+			JSONArray root = new JSONArray(json);
+			for (int i=0; i<root.length(); i++) {
+				JSONObject row = root.getJSONObject(i);
+				pstmt.setString(1, row.getString("Name"));
+				pstmt.setString(2, row.getString("Address"));
+				pstmt.setString(3, row.getString("Tel"));
+				pstmt.setString(4, row.getString("HostWords"));
+				pstmt.setString(5, row.getString("FoodFeature"));
+				pstmt.setString(6, row.getString("City"));
+				pstmt.setString(7, row.getString("Town"));
+				pstmt.setString(8, row.getString("Coordinate"));
+				pstmt.setString(9, row.getString("PicURL"));
+				pstmt.executeUpdate();
+			}
+			System.out.println("OK");
+		}catch (Exception e) {
+			System.out.println(e.toString());
 		}
-	}
-	
-	private static void insertOpendata() {
 		
 	}
+	
 
 }
